1397ad8158731494b2a765c7d626f4a3f4338982
shunmakk <shunmakajioka12@gmail.com>
2024-07-31 23:08:24 +0900

    投稿をリアルタイムにできるようにする

--- app/components/Post .tsx
+++ app/components/Post .tsx
@@ -1,6 +1,8 @@
 'use client'
-import { useOptimistic } from "react"
+import { useEffect, useOptimistic } from "react"
 import Likes from "./Likes"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 export default function Post({posts}: {posts: PostWithAuthor[]}) {
 
@@ -13,6 +15,18 @@ export default function Post({posts}: {posts: PostWithAuthor[]}) {
     newOptimisticPost[index] = newPost;
     return newOptimisticPost;
 });
+
+
+//リアルタイムに投稿が反映される supbaseのSubscribe to channelを参照
+const supbase = createClientComponentClient();
+const router = useRouter();
+
+useEffect(() => {
+  supbase.channel("何でも良い").on('postgres_changes', { event: '*', schema: 'public', table: 'Post' }, payload => {
+    console.log('Change received!', payload)
+  })
+  .subscribe()
+},[supbase,router])
 
 
 
