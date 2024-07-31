bc673af84091a56793c48bde0bf5915e5f286215
shunmakk <shunmakajioka12@gmail.com>
2024-07-31 21:49:19 +0900

    usetrainsitonでパフォーマンス改善

--- app/components/Likes.tsx
+++ app/components/Likes.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 
 type Post = Database["public"]["Tables"]["posts"]["Row"];
 type Profile = Database["public"]["Tables"]["profiles"]["Row"];
@@ -11,11 +12,10 @@ type PostWithAuthor = Post & {
     user_has_liked_post:boolean;
 }
 
-
-
 export default function Likes({post,addOptimisticPost}: {post: PostWithAuthor; addOptimisticPost:(newPost: PostWithAuthor) => void }){
 
     const router = useRouter()
+    const [isPending,startTransition] = useTransition();
 
     const handleClick = async() => {
         const supbase = createClientComponentClient();
@@ -25,7 +25,7 @@ export default function Likes({post,addOptimisticPost}: {post: PostWithAuthor; a
         if(post.user_has_liked_post){
             addOptimisticPost({
                 ...post, //既存のポスト
-                likes: post.likes + 1,  //lilesの属性を更新する -1
+                likes: post.likes - 1,  //lilesの属性を更新する -1
                 user_has_liked_post: !post.user_has_liked_post
             });
              await supbase.from('likes').delete().match({user_id: user?.id, post_id: post.id});
@@ -39,13 +39,10 @@ export default function Likes({post,addOptimisticPost}: {post: PostWithAuthor; a
              await supbase.from('likes').insert({user_id: user?.id, post_id: post.id});
              router.refresh()
         }
-
-
     }
 
-
     return(
-        <button onClick={handleClick}>{post.likes}いいね</button>
+        <button onClick={() => startTransition(() => handleClick())}>{post.likes}いいね</button>
     )
 
 }
